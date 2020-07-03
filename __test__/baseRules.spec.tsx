fe8c961abe4724e2e6f0b7c35821626cd56647e0
david32145 <nascimento32145@gmail.com>
2020-07-03 14:09:49 -0300

    Add render props

--- __test__/baseRules.spec.tsx
+++ __test__/baseRules.spec.tsx
@@ -6,7 +6,7 @@ import { ReactACLProvider, Can } from '@lib/index.ts'
 
 afterEach(cleanup)
 
-const UserList = () => (
+const UserList: React.FC = () => (
   <ul data-component="userlist">
     <li>User 1</li>
     <li>User 2</li>
@@ -40,6 +40,32 @@ const WithoutRoles: React.FC = () => {
   )
 }
 
+const WithoutRoleRenderProps: React.FC = () => {
+  return (
+    <ReactACLProvider
+      extractInitialRole={() => []}
+    >
+      <h1>Read an users</h1>
+      <Can resource="user" authority="read">
+        {accept => accept ? <p>true</p> : <p>false</p>}
+      </Can>
+    </ReactACLProvider>
+  )
+}
+
+const WithRoleRenderProps: React.FC = () => {
+  return (
+    <ReactACLProvider
+      extractInitialRole={() => ['user:read']}
+    >
+      <h1>Read an users</h1>
+      <Can resource="user" authority="read">
+        {accept => accept ? <p>true</p> : <p>false</p>}
+      </Can>
+    </ReactACLProvider>
+  )
+}
+
 describe('Main test', () => {
   test('it should not render <UserList />', () => {
     const { container } = render(<WithoutRoles />)
@@ -52,5 +78,21 @@ describe('Main test', () => {
     const userList = container.querySelector('[data-component="userlist"]')
     expect(userList).not.toBeNull()
     expect(userList?.querySelectorAll('li').length).toBe(3)
+  })
+
+  test('it should render <p>false</p> with render props', () => {
+    const { container } = render(<WithoutRoleRenderProps />)
+    const paragraphs = container.querySelectorAll('p')
+    expect(paragraphs.length).toBe(1)
+    expect(paragraphs[0]).not.toBeNull()
+    expect(paragraphs[0]).toHaveTextContent('false')
+  })
+
+  test('it should render <p>true</p> with render props', () => {
+    const { container } = render(<WithRoleRenderProps />)
+    const paragraphs = container.querySelectorAll('p')
+    expect(paragraphs.length).toBe(1)
+    expect(paragraphs[0]).not.toBeNull()
+    expect(paragraphs[0]).toHaveTextContent('true')
   })
 })
--- lib/Can.tsx
+++ lib/Can.tsx
@@ -5,16 +5,35 @@ const includesRule = (rule: Rule, rules: string[]): boolean => {
   return rules.includes(`${rule.resource}:${rule.authority}`)
 }
 
+function isCanRenderProps (children: CanChildren): children is CanRenderProps {
+  if (children instanceof Function) {
+    return true
+  }
+  return false
+}
+
+type CanRenderProps = (accept: boolean) => React.ReactElement
+
+type CanChildren = CanRenderProps | JSX.Element
+
 export interface CanProps {
   resource: string
   authority: string
+  children: CanChildren
 }
 
 const Can: React.FC<CanProps> = ({ children, ...rule }) => {
   const rules = useRules()
-  if (!includesRule(rule, rules)) {
+  const accept = includesRule(rule, rules)
+
+  if (isCanRenderProps(children)) {
+    return children(accept)
+  }
+
+  if (!accept) {
     return null
   }
+
   return <>{children}</>
 }
 
